d1c9275b802257288e88472cfb9cfb90d18bae86
ChilontheBW <ChilontheBW@gmail.com>
2018-11-13 17:58:02 -0800

    Update Install.cpp

--- Install.cpp
+++ Install.cpp
@@ -1,6 +1,8 @@
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+
+using namespace std;
  
 int main(){
 	try{
@@ -8,35 +10,53 @@ int main(){
 		std::system("wget -O /home/pi/Downloads/relays https://github.com/ChilontheBW/PiRelays/archive/master.zip");
 		
 		// creates the new directory if needed
+		bool cont = false;
+		
 		try{
-		std::system("sudo mkdir /home/pi/python/relays");
-		}catch (const std::exception &e){}
- 
-		// unpacks the new directory
-		std::system("sudo unzip /home/pi/Downloads/relays -d /home/pi/python/relays");
-		std::system("sudo rm -r /home/pi/Downloads/PiRelays-master");	
-		
-		// moves all needed program files and compiles the programmer
-		std::system("sudo mv /home/pi/python/relays/PiRelays-master/bin /home/pi/Downloads/relays");
-		std::system('sudo mv /home/pi/python/relays/PiRelays-master/"READ ME".text /home/pi/Downloads/relays');
-		std::system("sudo mv /home/pi/python/relays/PiRelays-master/config.ini /home/pi/Downloads/relays");
-		std::system("sudo g++ /home/pi/python/relays/PiRelays-master/RelayManager.cpp -o /home/pi/python/relays/emergencymanager");
-		// moves the vnc startup config file 
-		std::system("mv /home/pi/python/relays/PiRelays-master/tightvncserver.service /etc/systemd/system/");
-		
-		// installs the supporting programs
-		std::system("sudo apt-get install tightvncserver");
-		std::system("sudo apt-get install xrdp"); 
+			std::system("sudo mkdir /home/pi/python/relays");
+			cont = true;
+	
+		}catch (const std::exception &e){
+			char* input = new char[20];
+			std::cout << "Package already installed, do you want to continue install?";
+			std::cin >> input;
+	
+			if(input[0] == char('y')){	
+				cont = true;		
+			}
 
-		// gives the vnc start files permissions and adds it to the startup files		
-		std::system("sudo chown root:root /etc/systemd/system/tightvncserver.service");
-		std::system("sudo chmod 755 /etc/systemd/system/tightvncserver.service");
-		std::system("sudo systemctl enable tightvncserver.service");
+		delete [] input;
+		}
 
-		// removes all extra stuff	
-		std::system("sudo rm -r /home/pi/python/relays/PiRelays-master");	
+		
+
+		if(cont){
+			int inputsize = 13;
+			std::string cmds[inputsize] = {"sudo unzip /home/pi/Downloads/relays -d /home/pi/python/relays",
+				"sudo rm -r /home/pi/Downloads/PiRelays-master",
+				"sudo mv /home/pi/python/relays/PiRelays-master/bin /home/pi/Downloads/relays",
+				"sudo mv /home/pi/python/relays/PiRelays-master/'READ ME'.text /home/pi/Downloads/relays",
+				"sudo mv /home/pi/python/relays/PiRelays-master/config.ini /home/pi/Downloads/relays",
+				"sudo g++ /home/pi/python/relays/PiRelays-master/RelayManager.cpp -o /home/pi/python/relays/emergencymanager",
+				"mv /home/pi/python/relays/PiRelays-master/tightvncserver.service /etc/systemd/system/",
+				"sudo apt-get install tightvncserver",
+				"sudo apt-get install xrdp",
+				"sudo chown root:root /etc/systemd/system/tightvncserver.service",
+				"sudo chmod 755 /etc/systemd/system/tightvncserver.service",
+				"sudo systemctl enable tightvncserver.service",
+				"sudo rm -r /home/pi/python/relays/PiRelays-master"
+				}
+			for(int i = 0; i< inputsize; i++){
+			std::system(cmds[i]);
+			}	
+			std::exit(0);	
+ 		}
+		else{
+			std::exit(0);
+		}
 	}
 	catch(const std::exception &e){
 		std::cout << "Erorr in setup" << e.what() << std::endl;
+		std::exit(1);
 	}
 }
