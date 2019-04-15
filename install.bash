fd3aa84fb6fb27f371da4f34133a58ce81b85d7f
ChilontheBW <ChilontheBW@gmail.com>
2019-04-15 13:38:18 -0700

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,16 +1,12 @@
 filepath=/home/pi
 
 sudo apt-get update
-sudo apt-get install -y rsync
 
 #ufw
 sudo apt-get install -y ufw
 sudo ufw logging on
 sudo ufw enable
 sudo ufw allow ssh
-
-#ldap
-sudo rsync -Pavz --delete --rsync-path="sudo rsync"  KUOWhub:/mnt/pi "$(filepath)"
 
 #update scheduling
 sudo systemctl enable --now atd.service
