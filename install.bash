3a3f9313b7ffc29fad46f4906608dda1363b4337
ChilontheBW <ChilontheBW@gmail.com>
2019-04-15 16:51:48 -0700

    Update install.bash

--- install.bash
+++ install.bash
@@ -2,11 +2,18 @@ filepath=/home/pi
 
 sudo apt-get update
 
+#keygen and sync
+sudo mkdir -p /home/pi/.ssh
+sudo ssh-keygen -t rsa -b 4096 -v -f /home/pi/.ssh/sshkeygen -N ""
+sudo rsync -Pavz --delete --rsync-path="sudo rsync" /home/pi/.ssh/sshkeygen.pub 
+
+
 #ufw
 sudo apt-get install -y ufw
 sudo ufw logging on
 sudo ufw enable
 sudo ufw allow ssh
+
 
 #update scheduling
 sudo systemctl enable --now atd.service
